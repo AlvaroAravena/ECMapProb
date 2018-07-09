065d46b161bde56303b32c6ea788d4b8362d371a
AlvaroAravena <a.aravena.ponce@gmail.com>
2018-07-10 00:27:41 +0200

    New examples

--- EXAMPLES/Citlaltepetl/input_data.py
+++ EXAMPLES/Citlaltepetl/input_data.py
@@ -1,5 +1,5 @@
 # Name of the run (used to save the parameters and the output)
-run_name = Phlegrean_Fields
+run_name = Citlaltepetl
 
 # Source of DEM (1 => SRTM 30 m / 2 => uploaded DEM. A default location is assumed for uploaded DEMS: input_DEM.asc, see an example in EXAMPLES/UploadDEM)
 source_dem = 1
@@ -9,16 +9,16 @@ source_dem = 1
 # lon2 = longitude of the second limit of the map 
 # lat1 = latitude of the first limit of the map
 # lat2 = latitude of the second limit of the map
-lon1 = 13.9
-lon2 = 14.4
-lat1 = 40.6
-lat2 = 41.1
+lon1 = -97.5
+lon2 = -97.0
+lat1 = 18.9
+lat2 = 19.2
 
 # Number of cones
 cone_levels = 30
 
 # Probability distribution of collapse location (1 => Punctual / 2 => Linear / 3 => Circumference arch)
-dist_source = 3
+dist_source = 1
 
 # Parameters of the collapse location
 # lon_cen = longitude of the collapse zone center (only considered if source_dem = 1)
@@ -31,24 +31,19 @@ dist_source = 3
 # radius_rad = radius of the circumference arch that define the collapse zone (in meters, only considered if dist_source = 3)
 # ang1_rad = initial angle of the circumference arch that define the collapse zone (in degrees, only considered if dist_source = 3. Anticlockwise)
 # ang2_rad = initial angle of the circumference arch that define the collapse zone (in degrees, only considered if dist_source = 3. Anticlockwise)
-lon_cen = 14.14
-lat_cen = 40.85
+lon_cen = -97.268
+lat_cen = 19.030
 var_cen = 300.0
-radius_rad = 4500.0
-ang1_rad = -60.0
-ang2_rad = 240.0
 
 # Other parameters of energy cones
 # height = expected height of collapse (above the surface, in meters)
-# hl0 = initial H/L for the energy cones
-# hl = minimum H/L for the energy cones
+# hl = H/L for the energy cones
 # var_height = uncertainty of collapse height (in meters)
-# var_hl0 = uncertainty of hl0
 # var_hl = uncertainty of hl
-height = 500.0
+height = 300.0
 hl = 0.40
-var_height = 200.0
-var_hl = 0.05
+var_height = 100.0
+var_hl = 0.03
 
 # Number of energy cones computed by the code
 N = 100
